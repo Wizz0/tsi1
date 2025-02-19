82bd2ece4519e7eda2c72ea1d4ca3534c400595d
Semyon Gorokhov <samgorokhov01@gmail.com>
2025-02-20 08:11:58 +0900

    route edit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <a href='/'>Home</a>
+             <a href='/about'>About</a>
+             <a href='/contact'> Contact</a>
+            </div>
+            
+
+        )
+    }
+}
+
+//export default Header
